4fcf24aa2616f5fc60e7d00fc7f06d16eb30d2b9
AK Maurya <ankitkrmaurya61@gmail.com>
2024-01-06 14:09:45 +0530

    bug fix

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,29 @@
-const nav = document.getElementById('nav');
-const navbar = document.getElementById('navbar');
+const nav = document.getElementById("nav");
+const hambar = document.getElementById("hamburger");
+const navbar = document.getElementById("navbar");
+const bars = document.getElementById("bars");
 
 
+hambar.addEventListener('click', function(e){
+  
+  
+  if(navbar.style.display ='grid'){
+    bars.style.display = "none";
+    console.log("add menu");
+  }else{
+    bars.style.display = "display";
+    console.log("remove menu");
+  }
 
-nav.addEventListener("click", function (e) {
-  navbar.remove(navbar)
-  e.preventDefault();
-});
+  
+})
+
+nav.addEventListener('click', function(e){
+ if ((navbar.style.display = "none")) {
+     bars.style.display = "flex";
+     console.log("remove menu");
+   } else {
+     hambar.style.display = "none";
+     console.log("remove menu");
+   }
+})
